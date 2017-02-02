3d3cc919cd8823c80ed4422e2bbd8e1ed2df6f6d
erlangga87 <deva.erlangga87@gmail.com>
2017-02-02 09:52:53 +0700

    Initial Commit RecyclerView2

--- app/src/test/java/id/sch/smktelkom_mlg/learn/recyclerview2/ExampleUnitTest.java
+++ app/src/test/java/id/sch/smktelkom_mlg/learn/recyclerview2/ExampleUnitTest.java
@@ -1,13 +1,11 @@
-package id.sch.smktelkom_mlg.learn.recyclerview1;
+package id.sch.smktelkom_mlg.learn.recyclerview2;
 
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test
